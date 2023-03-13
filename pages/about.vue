25dc45854e0e678179860107739f97b3525a50bf
bokotomo <bokotomo@me.com>
2023-03-13 23:58:23 +0900

    feat

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <TemplateAbout />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TemplateAbout from '@/components/templates/About.vue';
+
+export default Vue.extend({
+  name: 'AboutPage',
+  components: { TemplateAbout },
+});
+</script>
